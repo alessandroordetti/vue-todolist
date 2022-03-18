a00cf8763ea0f09f7b3c63b4ea9c40b34a22cd7b
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-18 17:15:00 +0100

    Adding remove and add functions

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 const app = new Vue (
     {
         el: '#app',
-        data: 
-    }
-)
+        data: {
+            todoList: [],
+
+            nuovoContenutoTodo: "",
+        },
+
+        
+
+        methods: {
+            aggiungiTodoItem (contenutoTodo) {
+                if (contenutoTodo.trim() !== "") {
+                    this.todoList.push(contenutoTodo.trim());
+                    this.nuovoContenutoTodo = "";
+                } else {
+                    console.warn('Il pc sta per esplodere perché non puoi aggiungere questo Item!!!!!!')
+                }
+            },
+
+            rimuoviTodoItem (indiceTodo) {
+                if(this.todoList[indiceTodo] !== undefined){
+                    /* Rimuove un elemento all'indice todoIndice */
+                    this.todoList.splice(indiceTodo, 1);
+                }  else {
+                    console.warn('Il pc sta per esplodere perché non puoi rimuovere questo Item dato che non è presente nella todoList!!!!!!')
+                }
+            },
+        }
+    });
